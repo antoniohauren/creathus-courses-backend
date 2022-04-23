add1f49b9d0c2f79c5f6bbfec97d7a577d281758
Antonio Hauren <antoniohauren@gmail.com>
2022-04-23 14:39:06 -0300

    feat: add prisma service

---
+++ src/prisma/prisma.service.ts
@@ -0,0 +1,27 @@
+import {
+  INestApplication,
+  Injectable,
+  OnModuleDestroy,
+  OnModuleInit,
+} from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+
+@Injectable()
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
+  async onModuleDestroy() {
+    await this.$connect();
+  }
+
+  async enableShutdownHooks(app: INestApplication) {
+    this.$on('beforeExit', async () => {
+      await app.close();
+    });
+  }
+
+  async onModuleInit() {
+    await this.$disconnect();
+  }
+}
