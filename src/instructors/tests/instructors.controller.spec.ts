bb941cd94a440975f644357c40843f6c333989ce
Antonio Hauren <antoniohauren@gmail.com>
2022-04-23 17:23:24 -0300

    test: ensure that controller calls service (create)

--- src/instructors/tests/instructors.controller.spec.ts
+++ src/instructors/tests/instructors.controller.spec.ts
@@ -1,15 +1,26 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { PrismaService } from '../../prisma/prisma.service';
+import { Instructor } from '../entities/instructor.entity';
 import { InstructorsController } from '../instructors.controller';
 import { InstructorsService } from '../instructors.service';
 
 describe('InstructorsController', () => {
   let controller: InstructorsController;
 
+  const instructorServiceMock = {
+    create: jest.fn((data) => ({ id: 'any_id', ...data })),
+  };
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [InstructorsController],
-      providers: [InstructorsService, PrismaService],
+      providers: [
+        {
+          provide: InstructorsService,
+          useValue: instructorServiceMock,
+        },
+        PrismaService,
+      ],
     }).compile();
 
     controller = module.get<InstructorsController>(InstructorsController);
@@ -17,5 +28,18 @@ describe('InstructorsController', () => {
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  describe('create', () => {
+    it('Should create a new instructor', () => {
+      expect(
+        controller.create({ email: 'any_email', name: 'any_name' }),
+      ).toEqual(
+        expect.objectContaining({
+          email: 'any_email',
+          name: 'any_name',
+        }),
+      );
+    });
   });
 });
