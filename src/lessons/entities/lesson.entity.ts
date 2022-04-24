5dc2b8eef405a347ea7c55aae4d5a3c2863a9e5f
Antonio Hauren <antoniohauren@gmail.com>
2022-04-23 23:43:36 -0300

    feat: update entities

--- src/lessons/entities/lesson.entity.ts
+++ src/lessons/entities/lesson.entity.ts
@@ -1,1 +1,11 @@
-export class Lesson {}
+import { Instructor } from '../../instructors/entities/instructor.entity';
+
+export class Lesson {
+  id: string;
+  duration: number;
+
+  instructor: Instructor;
+
+  created_at: Date;
+  updated_at: Date;
+}
