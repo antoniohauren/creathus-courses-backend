0bb9445acdd515b2cc817583b9983ae74bae7a0a
Antonio Hauren <antoniohauren@gmail.com>
2022-04-23 23:36:34 -0300

    feat: add files generated by nest resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { InstructorsModule } from './instructors/instructors.module';
+import { LessonsModule } from './lessons/lessons.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), InstructorsModule],
+  imports: [ConfigModule.forRoot(), InstructorsModule, LessonsModule],
   controllers: [],
   providers: [],
 })
