10cd7695c5ad14609a19a823b0aa2cb4bda7d7fb
Antonio Hauren <antoniohauren@gmail.com>
2022-04-26 22:32:14 -0300

    test: ensure that controller calls service (create)

--- src/trails/tests/trails.controller.spec.ts
+++ src/trails/tests/trails.controller.spec.ts
@@ -1,15 +1,29 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { PrismaService } from '../../prisma/prisma.service';
+import { Trail } from '../entities/trail.entity';
 import { TrailsController } from '../trails.controller';
 import { TrailsService } from '../trails.service';
 
 describe('TrailsController', () => {
   let controller: TrailsController;
 
+  const TrailStub: Partial<Trail> = {
+    id: 'stub_id',
+    name: 'stub_name',
+    courses: [],
+  };
+
+  const TrailServiceMock = {
+    create: jest.fn((data) => ({ id: 'any_id', ...data })),
+  };
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [TrailsController],
-      providers: [TrailsService, PrismaService],
+      providers: [
+        { provide: TrailsService, useValue: TrailServiceMock },
+        PrismaService,
+      ],
     }).compile();
 
     controller = module.get<TrailsController>(TrailsController);
@@ -17,5 +31,15 @@ describe('TrailsController', () => {
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  describe('create', () => {
+    it('Should create a new instructor', () => {
+      expect(controller.create({ name: 'any_name' })).toEqual(
+        expect.objectContaining({
+          name: 'any_name',
+        }),
+      );
+    });
   });
 });
