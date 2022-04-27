55191057ec2c9e90dae7183502b56dd0543a2086
Antonio Hauren <antoniohauren@gmail.com>
2022-04-26 22:18:17 -0300

    test: ensure that create returns a valid trail

--- src/trails/tests/trails.service.spec.ts
+++ src/trails/tests/trails.service.spec.ts
@@ -5,9 +5,21 @@ import { TrailsService } from '../trails.service';
 describe('TrailsService', () => {
   let service: TrailsService;
 
+  const PrismaServiceMock = {
+    trail: {
+      create: jest.fn(({ data }) => ({
+        id: 'any_id',
+        ...data,
+      })),
+    },
+  };
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [TrailsService, PrismaService],
+      providers: [
+        TrailsService,
+        { provide: PrismaService, useValue: PrismaServiceMock },
+      ],
     }).compile();
 
     service = module.get<TrailsService>(TrailsService);
@@ -15,5 +27,24 @@ describe('TrailsService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('create', () => {
+    it('Should return an a new trail', async () => {
+      const createSpy = jest.spyOn(service, 'create');
+
+      const result = await service.create({
+        name: 'any_name',
+      });
+
+      expect(createSpy).toHaveBeenCalledWith({
+        name: 'any_name',
+      });
+      expect(result).toEqual(
+        expect.objectContaining({
+          name: 'any_name',
+        }),
+      );
+    });
   });
 });
